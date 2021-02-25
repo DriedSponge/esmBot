81838d808ca00138b7b7b60ffb79633bc9e32e77
DriedSponge <jordan@driedsponge.net>
2021-02-25 07:30:46 -0800

    Change invite link

--- commands/invite.js
+++ commands/invite.js
@@ -1,5 +1,5 @@
 exports.run = async (message) => {
-  return `${message.author.mention}, you can invite me to your server here: <https://projectlounge.pw/invite${process.env.NODE_ENV === "development" ? "dev" : ""}>`;
+  return `${message.author.mention}, you can invite me to your server here: <https://discord.com/api/oauth2/authorize?client_id=813807335502905344&permissions=2151013952&scope=bot>`;
 };
 
 exports.category = 1;
